a6936df1138fbcdf221d4528b51827eb19a62f2e
rivia mirelle <rubenvianadeoliveira@gmail.com>
2026-02-27 16:12:26 -0300

    Add files via upload

---
+++ UC - algoritmo (27-02-2026)/exemplo.py
@@ -0,0 +1,12 @@
+altura = float(input("digite a sua altura"))
+altura = altura * 100 
+
+print (f"Sua altura é de {altura} cm") 
+print (f"Sua altura é de: ", altura)
+
+nome = "Gaby"
+idade = 23 
+
+texto = "Meu nome é {} e tenho {} anos".format(nome, idade)
+print(texto)
+
