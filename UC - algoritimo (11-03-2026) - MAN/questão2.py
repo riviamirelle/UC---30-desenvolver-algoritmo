841c66ce0a6593a7033a0bcc455a795bc2063753
rivia mirelle <rubenvianadeoliveira@gmail.com>
2026-03-11 11:23:06 -0300

    Add files via upload

---
+++ UC - algoritimo (11-03-2026) - MAN/questão2.py
@@ -0,0 +1,11 @@
+distancia = 450
+consumoCarro = 8 
+precoLitro = 5.50
+
+litroConsumido = distancia / consumoCarro 
+custoTotal = litroConsumido * precoLitro 
+
+print("Distancia percorrida: ", distancia, "Km")
+print("Litros consumidos pelo carro: ", consumoCarro, "Km/L")
+print("Litros consumidos: ", litroConsumido)
+print("Custo total de combustivel: R$", custoTotal)
---
+++ UC - algoritimo (11-03-2026) - MAN/questão5.py
@@ -0,0 +1,13 @@
+nome = input ("digite o nome completo do aluno: ")
+
+matrícula = int(input("Qual o numero da sua matrícula? "))
+
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+
+media = (nota1 + nota2) / 2 
+
+print("NOME: ", nome)
+print("MATRÍCULA: ", matrícula)
+print("MÉDIA: ", media)
+
