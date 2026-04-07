170fe72a7990444ba567a9ba26b3264876970cb4
rivia mirelle <rubenvianadeoliveira@gmail.com>
2026-04-07 13:54:51 -0300

    Update atividade.py

--- UC - algoritmo (07.04.2026)/atividade.py
+++ UC - algoritmo (07.04.2026)/atividade.py
@@ -1,14 +1,14 @@
-R = int(input("Dê a quantidade de R: "))
-M = int(input("Dê a quantidade de M: "))
-V = int(input("Dê a quantidade de V: "))
+P = int(input("Dê a quantidade de P: "))
+D = int(input("Dê a quantidade de D: "))
+B = int(input("Dê a quantidade de B: "))
 
-total = R*1 + M*2 + V*3
+total = P*1 + D*2 + B*3
 
 if total >= 150:
-    print("R")
+    print("B")
 elif total >= 120:
-    print("M")
+    print("D")
 elif total >= 100:
-    print("V")
+    print("P")
 else:
-    print("N") 
+    print("N")
